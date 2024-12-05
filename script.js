607950872acc14f1e590df294febeaedac55f806
yelantsevv <yelantsevv@gmail.com>
2024-12-05 17:20:20 +0200

    add : input

--- script.js
+++ script.js
@@ -3,56 +3,59 @@ const second = document.querySelector(".second");
 const argument = document.querySelector(".argument");
 const result = document.querySelector(".result");
 const rez = document.querySelector(".rez");
-
+let answer;
+let r;
 const start = { from: 2, to: 10, argument: ["+", "-", "*", "/"] };
 
 const random = (x, y) => Math.floor(Math.random() * (y - x + 1)) + x;
 
+const active = (arg, className) => {
+    [first, second, argument, result][r].classList[arg](className);
+};
+
 const arg = {
-  "+": (x, y) => x + y,
-  "-": (x, y) => x - y,
-  "*": (x, y) => x * y,
-  "/": (x, y) => x / y,
+    "+": (x, y) => x + y,
+    "-": (x, y) => x - y,
+    "*": (x, y) => x * y,
+    "/": (x, y) => x / y,
 };
 
 function getArgument() {
-  const x = random(start.from, start.to);
-  const y = random(start.from, start.to);
-  const z = start.argument[random(0, start.argument.length - 1)];
-  const rez = arg[z](x, y);
-  if (parseInt(rez) !== rez || rez < 0) return getArgument();
-  if (z === "*" && Math.random() - 0.5 > 0 && start.argument.includes("/"))
-    return [rez, y, "/", x];
-  return [x, y, z, rez];
+    const x = random(start.from, start.to);
+    const y = random(start.from, start.to);
+    const z = start.argument[random(0, start.argument.length - 1)];
+    const rez = arg[z](x, y);
+    if (parseInt(rez) !== rez || rez < 0) return getArgument();
+    if (z === "*" && Math.random() - 0.5 > 0 && start.argument.includes("/"))
+        return [rez, y, "/", x];
+    return [x, y, z, rez];
 }
 
-let answer;
-let r;
 function varify() {
-  const rez = getArgument();
-  r = random(0, 3);
-  answer = rez[r];
-  rez[r] = "";
-  first.textContent = rez[0];
-  second.textContent = rez[1];
-  argument.textContent = rez[2];
-  result.textContent = rez[3];
-  [first, second, argument, result][r].classList.add("active");
+    const rez = getArgument();
+    r = random(0, 3);
+    answer = rez[r];
+    rez[r] = "";
+    first.textContent = rez[0];
+    second.textContent = rez[1];
+    argument.textContent = rez[2];
+    result.textContent = rez[3];
+    active("add", "active");
 }
 
 function results() {
-  let r = [answer];
-  while (r.length < 4) {
-    if (typeof answer === "string") {
-      r = ["+", "-", "*", "/"];
-      break;
+    let r = [answer];
+    while (r.length < 4) {
+        if (typeof answer === "string") {
+            r = ["+", "-", "*", "/"];
+            break;
+        }
+        const randomNumber = random(answer - 10, answer + 10);
+        if (r.includes(randomNumber)) continue;
+        r.push(randomNumber);
     }
-    const randomNumber = random(answer - 10, answer + 10);
-    if (r.includes(randomNumber)) continue;
-    r.push(randomNumber);
-  }
-  r.sort(() => Math.random() - 0.5);
-  rez.innerHTML = ` 
+    r.sort(() => Math.random() - 0.5);
+    rez.innerHTML = ` 
             <p>${r[0]}</p>
             <p>${r[1]}</p>
             <p>${r[2]}</p>
@@ -62,28 +65,31 @@ function results() {
 
 let flag = false;
 rez.addEventListener("mouseup", (e) => {
-  if (flag) return;
-  flag = true;
-  if (e.target.textContent == answer) {
-    [first, second, argument, result][r].innerHTML = e.target.textContent;
-    setTimeout(() => {
-      flag = false;
-      [first, second, argument, result][r].classList.remove("active");
-      varify();
-      results();
-      count(true);
-    }, 1000);
-  } else {
-    [first, second, argument, result][r].innerHTML = e.target.textContent;
-    [first, second, argument, result][r].classList.add("error");
-    setTimeout(() => {
-      flag = false;
-      [first, second, argument, result][r].innerHTML = "";
-      [first, second, argument, result][r].classList.remove("error");
-      results();
-      count(false);
-    }, 1000);
-  }
+    if (flag) return;
+    flag = true;
+    if (e.target.textContent == answer) {
+        [first, second, argument, result][r].innerHTML = e.target.textContent;
+        setTimeout(() => {
+            flag = false;
+            active("remove", "active");
+            varify();
+            results();
+            inp.value = "";
+            inp.focus();
+            // count(true);
+            rez.classList.add("hidden");
+        }, 1000);
+    } else {
+        [first, second, argument, result][r].innerHTML = e.target.textContent;
+        active("add", "error");
+        setTimeout(() => {
+            flag = false;
+            [first, second, argument, result][r].innerHTML = "";
+            active("remove", "error");
+            results();
+            count(false);
+        }, 1000);
+    }
 });
 varify();
 results();
@@ -94,37 +100,66 @@ const saveBtn = document.querySelector(".saveBtn");
 const form = document.querySelector("form");
 
 settings.addEventListener("click", (event) => {
-  event.preventDefault();
-  form.classList.toggle("hidden");
+    event.preventDefault();
+    form.classList.toggle("hidden");
 });
 saveBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  const [a, b, c, d, e, f] = [...event.target.parentElement];
-  start.from = +a.value;
-  start.to = +b.value;
-  start.argument = [];
-  if (c.checked) start.argument.push("+");
-  if (d.checked) start.argument.push("-");
-  if (e.checked) start.argument.push("*");
-  if (f.checked) start.argument.push("/");
+    event.preventDefault();
+    const [a, b, c, d, e, f] = [...event.target.parentElement];
+    start.from = +a.value;
+    start.to = +b.value;
+    start.argument = [];
+    if (c.checked) start.argument.push("+");
+    if (d.checked) start.argument.push("-");
+    if (e.checked) start.argument.push("*");
+    if (f.checked) start.argument.push("/");
 
-  form.classList.toggle("hidden");
+    form.classList.toggle("hidden");
 });
 
 const counter = document.querySelector(".count");
 function count(args = true) {
-  const counts = counter.textContent;
-  if (args) {
-    localStorage.setItem("counter", +counts + 1);
-  }
-  if (!args) {
-    localStorage.setItem("counter", +counts - 1);
-  }
-  counter.textContent = localStorage.getItem("counter");
+    const counts = counter.textContent;
+    if (args) {
+        localStorage.setItem("counter", +counts + 1);
+    }
+    if (!args) {
+        localStorage.setItem("counter", +counts - 1);
+    }
+    counter.textContent = localStorage.getItem("counter");
 }
 counter.textContent = localStorage.getItem("counter");
 
 counter.addEventListener("click", () => {
-  localStorage.setItem("counter", 0);
-  counter.textContent = localStorage.getItem("counter");
+    localStorage.setItem("counter", 0);
+    counter.textContent = localStorage.getItem("counter");
+});
+
+//-------- форма ответа --------
+const inp = document.querySelector(".inp");
+const btn_check = document.querySelector(".btn_check");
+inp.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        btn_check.click();
+    }
+});
+btn_check.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(inp.value, answer);
+    if (inp.value == answer) {
+        active("remove", "active");
+
+        results();
+        varify();
+        count(true);
+        inp.value = "";
+        inp.focus();
+        rez.classList.add("hidden");
+    } else {
+        results();
+        count(false);
+        inp.focus();
+        rez.classList.remove("hidden");
+    }
 });
